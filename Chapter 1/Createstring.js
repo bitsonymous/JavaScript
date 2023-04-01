a3eb5dbeba208e7d2d33e4742dfe0f4877e6d692
bitsonymousm <bitsonymous@gmail.com>
2023-04-01 15:48:03 +0530

    jaavscript file have been added

---
+++ Chapter 1/Createstring.js
@@ -0,0 +1,25 @@
+// Q1
+let a = "Himanshu"
+// Q2
+let b = a+54;
+console.log(a+4);
+// Q3
+console.log(typeof (b)); 
+// Q4
+const obj = {
+    name: "Himanshu",
+    RollNo: 71,
+    isPresent: true
+}
+// const obj = 45;
+// Q5
+obj['name'] = "Ravi";
+obj['friend'] = "payal";
+console.log(obj);
+const dic = {
+    creatively: "in an original or imaginative way",
+    sacrament: "(in the Christian Church) a religious ceremony or ritual regarded as imparting divine grace, such as baptism, the Eucharist and (in the Roman Catholic and many Orthodox Churches) penance and the anointing of the sick.",
+    cohort:"a group of people with a shared characteristic."
+}
+console.log(dic.creatively);
+console.log(dic);
