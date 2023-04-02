16c35917ad69867d3ab42ff5bd45835edd00810c
bitsonymousm <bitsonymous@gmail.com>
2023-04-02 23:14:46 +0530

    Added Chapter 2

---
+++ Chapter 2/Conditionalstmen.js
@@ -0,0 +1,8 @@
+let a = prompt("Enter Number")
+a = Number.parseInt(a)// conerting string into number
+if(a>10){
+    alert("This number is greater than 10")
+}
+else{
+    alert("This number is less 10")
+}
